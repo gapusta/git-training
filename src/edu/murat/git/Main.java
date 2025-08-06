e6660c10b0cb384c75dcde257a1443d6819a5c82
mkurmanbekov <m.kurmanbekov@wb-digital.ru>
2025-08-06 20:17:20 +0600

    [Feature] закончила реализовывать минус

--- src/edu/murat/git/Main.java
+++ src/edu/murat/git/Main.java
@@ -3,9 +3,14 @@ package edu.murat.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println(sum(13,15));
+        System.out.println(sub(333,222));
     }
 
     public static int sum(int x, int y) {
         return x + y;
+    }
+
+    public static int sub(int x, int y) {
+        return x - y;
     }
 }
