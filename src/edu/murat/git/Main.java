fb99fb94ce16f15b2dd8ee40649e8a3152459fc2
mkurmanbekov <m.kurmanbekov@wb-digital.ru>
2025-08-06 20:01:20 +0600

    [Feature] Addition

--- src/edu/murat/git/Main.java
+++ src/edu/murat/git/Main.java
@@ -2,6 +2,10 @@ package edu.murat.git;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(sum(13,15));
+    }
 
+    public static int sum(int x, int y) {
+        return x + y;
     }
 }
